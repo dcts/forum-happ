e4d984b752b5a7a131963d78fda0b574f1a9e61f
dcts <thomas@liist.com>
2022-07-18 17:59:50 +0200

    :white_check_mark: Excercise=3 Step=1 green

--- dna/zomes/posts/src/lib.rs
+++ dna/zomes/posts/src/lib.rs
@@ -1,5 +1,16 @@
 use hdk::prelude::*;
 
+#[hdk_entry_helper]
+struct Post {
+  title: String,
+  content: String,
+}
+
+#[hdk_entry_defs]
+#[unit_enum(UnitTypes)]
+enum EntryTypes {
+  Post(Post)
+}
 
 /**
  * DON'T TOUCH
